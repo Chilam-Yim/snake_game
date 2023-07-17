aae55801a99db4083f47addc899fc9427f99cdef
bruceyim <bruceyim@localhost>
2023-07-17 12:43:42 -0400

    first-commit

---
+++ src/GameFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	GameFrame(){
+		
+		GamePanel panel = new GamePanel();
+		this.add(panel); 
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(true);
+		this.pack();
+		//this.setSize(1080,720);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
+
+
+//Press any key to restart
+//pause and resume button (clicking)
+//321 countdown
+//bomb
+//acceleration
+//deceleration
+//constantly change bg
+//audio
+//no wall
---
+++ src/Snake.java
@@ -0,0 +1,11 @@
+
+public class Snake {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GameFrame frame = new GameFrame();
+
+	}
+
+}
