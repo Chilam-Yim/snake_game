0be6bed4126ec4197b9c4b3feaa6e2d74562981f
bruceyim <bruceyim@localhost>
2023-07-17 12:58:39 -0400

    commit2

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,7 +6,7 @@ public class GameFrame extends JFrame{
 		
 		GamePanel panel = new GamePanel();
 		this.add(panel); 
-		this.setTitle("Snake Game");
+		this.setTitle("The Snake Game");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(true);
 		this.pack();
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,7 +13,7 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel implements ActionListener {
 
 	static final int SCREEN_HEIGHT = 600;
-	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_WIDTH = 601;
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT/UNIT_SIZE);
 	final int x[] = new int[GAME_UNITS];
